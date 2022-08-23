5fd44fcb008069be051c82e26a5310f2d6556482
gonza7795 <marianotrojas8@gmail.com>
2022-08-23 19:29:09 -0300

    initial commit

---
+++ src/test/java/com/portfolio/gea/GeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.gea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
